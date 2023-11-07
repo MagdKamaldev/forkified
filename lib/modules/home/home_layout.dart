9aa57e75a8d5e13aea90b2da3a1f173a05625661
MagdKamaldev <magddevf@gmail.com>
2023-11-07 19:36:27 +0200

    home layout ui

--- lib/modules/home/home_layout.dart
+++ lib/modules/home/home_layout.dart
@@ -17,6 +17,31 @@ class _HomeLayoutState extends State<HomeLayout> {
   int currentpage = 0;
   int bottomNavBarIndex = 0;
 
+  List<RecipeCategory> categories = [
+    RecipeCategory(
+      name: "Italian Cuisine",
+      image: "assets/images/leaning-tower-of-pisa.png",
+    ),
+
+// Asian Cuisine
+    RecipeCategory(
+      name: "Asian Cuisine",
+      image: "assets/images/asia.png",
+    ),
+
+// Mediterranean Cuisine
+    RecipeCategory(
+      name: "Mediterranean Cuisine",
+      image: "assets/images/olive-oil.png",
+    ),
+
+// Comfort Food
+    RecipeCategory(
+      name: "Comfort Food",
+      image: "assets/images/cookie.png",
+    )
+  ];
+
   List<BannerItem> banners = [
     BannerItem(
         image:
@@ -62,77 +87,121 @@ class _HomeLayoutState extends State<HomeLayout> {
       ),
       drawer: const AppDrawer(),
       body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Text(
-                  "Trending Recipes",
-                  style: theme.displayLarge,
-                ),
-                SizedBox(
-                  width: size.width * 0.02,
-                ),
-                Icon(
-                  Icons.trending_up_sharp,
-                  color: platinum,
-                  size: size.width * 0.07,
-                )
-              ],
-            ),
-            SizedBox(
-              height: size.height * 0.02,
-            ),
-            ClipRRect(
-              borderRadius: BorderRadius.circular(12),
-              child: CarouselSlider(
-                  carouselController: _carouselController,
-                  items: banners
-                      .map(
-                        (e) => Image(
-                          image: NetworkImage(e.image),
-                          width: double.infinity,
-                          fit: BoxFit.cover,
-                        ),
-                      )
-                      .toList(),
-                  options: CarouselOptions(
-                    height: 250.0,
-                    onPageChanged: (index, reason) {
-                      setState(() {
-                        currentpage = index;
-                      });
-                    },
-                    initialPage: 0,
-                    viewportFraction: 1.0,
-                    enableInfiniteScroll: true,
-                    reverse: false,
-                    autoPlay: true,
-                    autoPlayInterval: const Duration(seconds: 3),
-                    autoPlayAnimationDuration: const Duration(seconds: 1),
-                    autoPlayCurve: Curves.fastOutSlowIn,
-                    scrollDirection: Axis.horizontal,
-                  )),
-            ),
-            SizedBox(
-              height: size.height * 0.02,
-            ),
-            DotsIndicator(
-              dotsCount: banners.length,
-              position: currentpage,
-              decorator: DotsDecorator(
-                color: cerulian,
-                activeColor: nonPhotoBlue,
-                spacing: const EdgeInsets.all(6.0),
-                activeSize: const Size(9.5, 9.5),
-                size: const Size(7.0, 7.0),
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: size.height * 0.02,
               ),
-            ),
-          ],
+              Row(
+                children: [
+                  Text(
+                    "Trending Recipes",
+                    style: theme.displayLarge,
+                  ),
+                  SizedBox(
+                    width: size.width * 0.02,
+                  ),
+                  Icon(
+                    Icons.trending_up_sharp,
+                    color: platinum,
+                    size: size.width * 0.07,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: CarouselSlider(
+                    carouselController: _carouselController,
+                    items: banners
+                        .map(
+                          (e) => Image(
+                            image: NetworkImage(e.image),
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        )
+                        .toList(),
+                    options: CarouselOptions(
+                      height: 250.0,
+                      onPageChanged: (index, reason) {
+                        setState(() {
+                          currentpage = index;
+                        });
+                      },
+                      initialPage: 0,
+                      viewportFraction: 1.0,
+                      enableInfiniteScroll: true,
+                      reverse: false,
+                      autoPlay: true,
+                      autoPlayInterval: const Duration(seconds: 3),
+                      autoPlayAnimationDuration: const Duration(seconds: 1),
+                      autoPlayCurve: Curves.fastOutSlowIn,
+                      scrollDirection: Axis.horizontal,
+                    )),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Center(
+                child: DotsIndicator(
+                  dotsCount: banners.length,
+                  position: currentpage,
+                  decorator: DotsDecorator(
+                    color: cerulian,
+                    activeColor: nonPhotoBlue,
+                    spacing: const EdgeInsets.all(6.0),
+                    activeSize: const Size(9.5, 9.5),
+                    size: const Size(7.0, 7.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Categroies",
+                    style: theme.displayLarge,
+                  ),
+                  SizedBox(
+                    width: size.width * 0.02,
+                  ),
+                  Icon(
+                    Icons.category,
+                    color: platinum,
+                    size: size.width * 0.07,
+                  )
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.03,
+              ),
+              GridView.count(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                crossAxisCount: 2,
+                crossAxisSpacing: size.width * 0.14,
+                mainAxisSpacing: size.height * 0.02,
+                childAspectRatio: 1 / 1,
+                children: List.generate(
+                  categories.length,
+                  (index) => buildCategory(
+                      categories[index], context, index, size, theme),
+                ),
+              )
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        iconSize: size.width * 0.07,
         backgroundColor: cerulian,
         selectedItemColor: platinum,
         currentIndex: bottomNavBarIndex,
@@ -140,8 +209,7 @@ class _HomeLayoutState extends State<HomeLayout> {
         items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(icon: Icon(Icons.add), label: "New"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person_outline), label: "User"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "User"),
         ],
         onTap: (index) {
           setState(() {
@@ -151,9 +219,42 @@ class _HomeLayoutState extends State<HomeLayout> {
       ),
     );
   }
+
+  Widget buildCategory(
+          RecipeCategory model, context, index, Size size, TextTheme theme) =>
+      GestureDetector(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.asset(
+              model.image,
+              width: size.width * 0.35,
+              height: size.height * 0.1,
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Text(
+              model.name,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: theme.displayMedium,
+            )
+          ],
+        ),
+      );
 }
 
 class BannerItem {
   final String image;
   BannerItem({required this.image});
 }
+
+class RecipeCategory {
+  final String name;
+  final String image;
+
+  RecipeCategory({required this.name, required this.image});
+}
+
+// Italian Cuisine
